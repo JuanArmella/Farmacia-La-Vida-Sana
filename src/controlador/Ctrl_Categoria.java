7ec281d53ad8c28e9a4296207e72d9eaf0fb500f
juanarmella <armellajuanmanuel123@gmail.com>
2025-03-28 17:34:12 -0500

    MVC

---
+++ src/controlador/Ctrl_Categoria.java
@@ -0,0 +1,13 @@
+package controlador;
+
+public class Ctrl_Categoria {
+    
+    /*
+
+    public boolean guardar (Categoria objeto) {
+        boolean respuesta = false;
+        Connection cn = conexion.Conexion.conectar();
+    }
+    
+    */ // PENDIENTE
+}

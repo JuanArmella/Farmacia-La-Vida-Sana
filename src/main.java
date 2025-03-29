1034d58bc92e2552b611a5587b00f29318d189bc
Miguel <caicedocervantes385@gmail.com>
2025-03-29 12:16:46 -0500

    Revert "Conexion BD"
    This reverts commit 7879684e7dbf4094468e24ec130047dada9067c6.

--- src/main.java
+++ src/main.java
@@ -1,13 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
-import vista.login;
-
-
-
+/**
+ *
+ * @author RYZEN
+ */
 public class main {
     
      public static void main(String[] args) {
-        login ventana1 = new login();
-        ventana1.setVisible(true);
         System.out.println("¡Hola, NetBeans!");
     }
      
